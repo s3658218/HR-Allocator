3dfbc4e5da39c59c5525700bff52d4169e4043e0
s3658218 <38154453+s3658218@users.noreply.github.com>
2018-04-07 19:50:06 +1000

    Add files via upload

---
+++ User_Interface.java
@@ -0,0 +1,52 @@
+import accounts.*;
+import request.*;
+import reports.*;
+import schedualing.*;
+
+public class User_Interface{
+	//add variable to refrence current account
+	public Accounts accounts;//list of all accounts
+	public RequestHandler requestHandler;//list of requests
+	public Schedual schedual;//schedual class
+	public Logs logs;//log of all past reports
+	
+	public User_Interface() {
+		StartUp();
+	}
+	
+	public void ShowSchedual(Schedual schedual) {
+		//formats schedual into readable data
+	}
+	public void ShowTimetable(Staff staff) {
+		//formats timetable for current account
+	}
+	public void ShowRequests(RequestHandler requestHandler) {
+		//formats requests for current account
+		//test
+	}
+	public void login() {
+		//walk through the login process
+		//get login(search through all accounts)
+		//get password(login function from account)
+	}
+	public void CreateRequest() {
+		//process of creating a job assignment request
+	}
+	public void CreateReport() {
+		//generates a report of system
+	}
+	public void ShowMenu(Account account) {
+		//shows menu of options for each account
+	}
+	public void StartUp() {
+		//for first time initialisation 
+		accounts = new Accounts();
+		requestHandler = new RequestHandler();
+		schedual = new Schedual();
+		logs = new Logs();
+	}
+	//utility methods
+	private void AddAccount(Account account) {
+		accounts.addAccount(account);//adds account to the list
+	}
+}
